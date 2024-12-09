6890dfd382b11c780cb0df7574e14bf99fef9a8b
DUPROS Leegan <leegan.dupros@epitech.eu>
2024-12-09 22:55:42 +0100

    Initial Commit

---
+++ Collection Manager/Collection_ManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Collection_ManagerApp.swift
+//  Collection Manager
+//
+//  Created by dleegan on 09/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct Collection_ManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
